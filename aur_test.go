4697a1d030fe73f07cddfb40abe1137e0e97abb2
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2018-10-07 16:27:23 +0200

    Use Go modules
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- aur_test.go
+++ aur_test.go
@@ -14,27 +14,27 @@ func TestGetDeps(t *testing.T) {
 	deps := make(map[string]struct{})
 
 	err := a.getDeps([]string{"sway-git"}, deps)
-	assert.NoError(t, err, "should not fail")
-	assert.Equal(t, 2, len(deps), "should have 2 elements")
+	assert.NoError(t, err)
+	assert.Len(t, deps, 4)
 	_, ok := deps["sway-git"]
-	assert.True(t, ok, "should be true")
-	_, ok = deps["wlc-git"]
-	assert.True(t, ok, "should be true")
+	assert.True(t, ok)
+	_, ok = deps["wlroots-git"]
+	assert.True(t, ok)
 
 	deps = make(map[string]struct{})
 	err = a.getDeps([]string{"virtualbox-host-modules-mainline"}, deps)
-	assert.NoError(t, err, "should not fail")
-	assert.Equal(t, 2, len(deps), "should have 2 elements")
+	assert.NoError(t, err)
+	assert.Len(t, deps, 2)
 
 	err = os.RemoveAll(baseDir)
-	assert.NoError(t, err, "should not fail")
+	assert.NoError(t, err)
 }
 
 // Test Getting sources from AUR.
 func TestAURGet(t *testing.T) {
 	_, err := a.Get([]string{"sway-git"})
-	assert.NoError(t, err, "should not fail")
+	assert.NoError(t, err)
 
 	err = os.RemoveAll(baseDir)
-	assert.NoError(t, err, "should not fail")
+	assert.NoError(t, err)
 }
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/mikkeloscar/maze-build
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/kr/pty v1.1.3
+	github.com/mikkeloscar/aur v0.0.0-20180725140122-f998dbf94dc4
+	github.com/mikkeloscar/gopkgbuild v0.0.0-20180615131628-2bb4f1f1db67
+	github.com/mikkeloscar/maze v0.0.0-20181007142116-7cc8eb14ba93
+	github.com/sirupsen/logrus v1.1.0
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/crypto v0.0.0-20181001203147-e3636079e1a4 // indirect
+	golang.org/x/oauth2 v0.0.0-20181003184128-c57b0facaced
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
